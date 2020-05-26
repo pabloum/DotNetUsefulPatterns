3b139e6f055fe4883a7f099d94e1492f609e2fa5
Pablo Uribe Montoya <pablo.um00@gmail.com>
2020-05-26 18:33:18 -0500

    Added new projects to sln. Implementation is not ready

---
+++ AccessAwsS3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AccessAwsS3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SqlBulk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqlBulk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
