54e8d79ad79eb99b25d989be5f1e74f47afb30b8
Pablo Uribe Montoya <pablo.um00@gmail.com>
2020-05-26 19:26:31 -0500

    Saga pattern skeleton generated. Warning: Code here does not compile

---
+++ SagaPattern/MySagaData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SagaPattern
+{
+    public class MySagaData
+    {
+        public String Property1 { get; set; }
+        public IList<int> Property2 { get; set; }
+        public IDictionary<String, String> DirectoryContent { get; set; }
+    }
+}
---
+++ SagaPattern/MySagaWorkflow.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+
+namespace SagaPattern
+{
+    public class MySagaWorkflow : IWorkflow<MySagaData>
+    {
+        public string Id => "PablosSagaPattern";
+
+        public int Version => 1;
+
+        public void Build(IWorkflowBuilder<MySagaData> builder)
+        {
+            // Structure
+            // builder.StartWith().Saga().Then().OnError();
+
+            builder.
+                StartWith(context => Console.WriteLine("Begin Saga"))
+                .Saga(saga => saga
+                    .StartWith<>()
+                    .Input(t => t.foo, data => data.boo)
+                    .Input(t => t.foo, data => data.boo)
+                    .Output(data => data.boo, step => step.Output)
+                    .CompensateWith<>()
+
+                    .Then<>()
+                    .Input(t => t.foo, data => data.boo)
+                    .Input(t => t.foo, data => data.boo)
+                    .Output(data => data.boo, step => step.Output)
+                    .CompensateWith<>()
+
+                    .Then<>()
+                    .Input(t => t.foo, data => data.boo)
+                    .Input(t => t.foo, data => data.boo)
+                    .Output(data => data.boo, step => step.Output)
+                    .CompensateWith<>()
+
+                    .Then<>()
+                    .Input(t => t.foo, data => data.boo)
+                    .Input(t => t.foo, data => data.boo)
+                )
+                .Then(context =>
+                    {
+                        Console.WriteLine("Workflow complete");
+                        return ExecutionResult.Next();
+                    })
+                .OnError(WorkflowErrorHandling.Terminate);
+        }
+    }
+}
